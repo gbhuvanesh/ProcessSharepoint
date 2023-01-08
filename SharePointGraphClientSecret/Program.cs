4c6f32f4dfabb06adf1c97e10ca06e1d9840ad9d
Gopalan Bhuvanesh <gopalanbhuvanesh@outlook.com>
2023-01-08 00:42:42 +0000

    New folder is created

--- SharePointGraphClientSecret/Program.cs
+++ SharePointGraphClientSecret/Program.cs
@@ -1,4 +1,5 @@
 ﻿//// See https://aka.ms/new-console-template for more information
+using Microsoft.Graph;
 using SharePointGraphClientSecret;
 
 Console.WriteLine("Hello, World!");
@@ -15,6 +16,16 @@ var site = task.GetAwaiter().GetResult();
 //var taskDrive = graphClient.Sites[site.Id].Drives["Letter"].Request().GetAsync();
 var taskDrive = graphClient.Sites[site.Id].Lists["Letter"].Request().GetAsync();
 var drive = taskDrive.GetAwaiter().GetResult();
+
+var newFolder = new DriveItem
+{
+    Name = "newFolder" + DateTime.Now.ToString("yyyyMMddhhmmss"),
+    Folder = new Folder()
+};
+
+var taskDriveLetter = graphClient.Sites[site.Id].Lists["Letter"].Drive.Root.Children.Request().AddAsync(newFolder);
+var driveLetter = taskDriveLetter.GetAwaiter().GetResult();
+
 
 Console.WriteLine($"Site Id {site.Id}");
 Console.WriteLine("Please press any key to exit");
