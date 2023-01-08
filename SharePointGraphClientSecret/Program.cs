ce13004f9d3d4ce0028ef025ed873793062e43af
Gopalan Bhuvanesh <gopalanbhuvanesh@outlook.com>
2023-01-08 00:21:52 +0000

    get the drive

--- SharePointGraphClientSecret/Program.cs
+++ SharePointGraphClientSecret/Program.cs
@@ -8,8 +8,14 @@ var graphClient = (new GraphCServicelientHelper()).GetGraphClient();
 
 //var me = await graphClient.Me.Request().GetAsync();
 
-var task = graphClient.Sites["root"].SiteWithPath("/sites/CRMDEV").Request().GetAsync();
+var task = graphClient.Sites["kamalacon.sharepoint.com:/sites/CRMDEV/"].Request().GetAsync();
+//var task = graphClient.Sites["root"].SiteWithPath("/sites/CRMDEV").Request().GetAsync();
 var site = task.GetAwaiter().GetResult();
+
+//var taskDrive = graphClient.Sites[site.Id].Drives["Letter"].Request().GetAsync();
+var taskDrive = graphClient.Sites[site.Id].Lists["Letter"].Request().GetAsync();
+var drive = taskDrive.GetAwaiter().GetResult();
+
 Console.WriteLine($"Site Id {site.Id}");
 Console.WriteLine("Please press any key to exit");
 Console.ReadKey();
